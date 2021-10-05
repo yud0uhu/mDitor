1b7d796f369d0fec93ec6fd02e3cc712daedad4d
yud0uhu <cist.b219.y.osaka@gmail.com>
2021-10-06 00:59:06 +0900

    font-sizeユーティリティを使用

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require("tailwindcss/plugin");
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +8,16 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme("fontSize.2xl") },
+        h2: { fontSize: theme("fontSize.xl") },
+        h3: { fontSize: theme("fontSize.lg") },
+        h4: { fontSize: theme("fontSize.base") },
+        h5: { fontSize: theme("fontSize.sm") },
+        h6: { fontSize: theme("fontSize.xs") },
+      });
+    }),
+  ],
 };
