c5aa938b8a5507ef412194c8660e10012a5a2f29
yud0uhu <denham@Yus-MacBook-Pro.local>
2022-09-24 19:56:44 +0900

    feat: viewを作成

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -38,19 +38,13 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"
               />
             </svg>
           </button>
 
           <button
+            @click="viewMode"
             class="border-solid border-2 rounded-md shadow-md text-center px-2 py-2 hover:bg-gray-50"
           >
             <svg
@@ -64,7 +58,14 @@
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M9 17V7m0 10a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h2a2 2 0 012 2m0 10a2 2 0 002 2h2a2 2 0 002-2M9 7a2 2 0 012-2h2a2 2 0 012 2m0 10V7m0 10a2 2 0 002 2h2a2 2 0 002-2V7a2 2 0 00-2-2h-2a2 2 0 00-2 2"
+                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
               />
             </svg>
           </button>
@@ -112,6 +113,42 @@
 
     <div>
       <div
+        class="grid grid-cols-3 gap-4"
+        v-show="!isVisibleEditor && !isVisiblePreview && isVisibleView"
+      >
+        <!-- ツールバー -->
+        <div>
+          <div v-if="this.source.match('#')">
+            <Markdown
+              :source="h1"
+              :plugins="plugins"
+              v-on="h1RegExp(this.source)"
+            />
+          </div>
+          <div v-if="this.source.match('##')">
+            <Markdown
+              :source="h2"
+              :plugins="plugins"
+              v-on="h2RegExp(this.source)"
+            />
+          </div>
+          <div v-if="this.source.match('###')">
+            <Markdown
+              :source="h3"
+              :plugins="plugins"
+              v-on="h3RegExp(this.source)"
+            />
+          </div>
+        </div>
+        <div class="view col-span-2">
+          <Markdown
+            :source="source"
+            :plugins="plugins"
+            class="font-body px-3 py-2 focus:outline-none w-full h-screen"
+          />
+        </div>
+      </div>
+      <div
         class="grid gap-1 grid-cols-2"
         v-show="isVisibleEditor && isVisiblePreview"
       >
@@ -138,7 +175,7 @@
       </div>
     </div>
 
-    <div class="editor" v-show="!isVisibleEditor">
+    <div class="editor" v-show="!isVisibleEditor && !isVisibleView">
       <textarea
         v-model="source"
         class="font-body shadow-inner bg-gray-900 text-white px-3 py-2 focus:outline-none shadow-2xl w-full h-screen"
@@ -149,7 +186,7 @@
       </textarea>
     </div>
 
-    <div class="preview" v-show="!isVisiblePreview">
+    <div class="preview" v-show="!isVisiblePreview && !isVisibleView">
       <Markdown
         :source="source"
         :plugins="plugins"
@@ -183,8 +220,13 @@ export default {
   data() {
     return {
       source: "",
+      h1: "",
+      h2: "",
+      h3: "",
+      regexp: "",
       isVisibleEditor: true,
       isVisiblePreview: true,
+      isVisibleView: false,
       plugins: [
         {
           plugin: MarkdownItStrikethroughAlt,
@@ -226,6 +268,33 @@ export default {
     },
     previewMode: function () {
       this.isVisiblePreview = !this.isVisiblePreview;
+    },
+    viewMode: function () {
+      this.isVisibleEditor = false;
+      this.isVisiblePreview = false;
+      this.isVisibleView = !this.isVisibleView;
+    },
+    h1RegExp: function (h1) {
+      this.regex = RegExp("[¥n/  ]");
+      this.h1 =
+        "# " +
+        h1.substring(this.source.indexOf("#")).split(this.regex)[2].toString();
+      // console.log(this.m1);
+    },
+    h2RegExp: function (h2) {
+      this.regex = RegExp("[¥n/  ]");
+      this.h2 =
+        "## " +
+        h2.substring(this.source.indexOf("##")).split(this.regex)[1].toString();
+    },
+    h3RegExp: function (h3) {
+      this.regex = RegExp("[¥n/  ]");
+      this.h3 =
+        "### " +
+        h3
+          .substring(this.source.indexOf("###"))
+          .split(this.regex)[1]
+          .toString();
     },
   },
   // mounted は永続されたデータがあればそれを読み込む
